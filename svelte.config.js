12fd7e8002f156f02dc405558ce6eb259692bd18
rjhoskins <robertjhoskins@gmail.com>
2023-07-05 22:28:39 -0700

    add adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -17,7 +17,7 @@ const config = {
 				holdMode: true,
 			}
 		}
-	}		
+	}
 
 };
 
